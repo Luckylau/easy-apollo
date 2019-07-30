d0e816dc1603bec8215deb9aa3e345324aa725c6
luckylau <laujunbupt0203@163.com>
2019-07-30 09:05:58 +0800

    common

---
+++ easy-apollo-common/src/main/java/lucky/apollo/common/constant/ReleaseOperationContext.java
@@ -0,0 +1,15 @@
+package lucky.apollo.common.constant;
+
+
+/**
+ * @Author luckylau
+ * @Date 2019/7/25
+ */
+public class ReleaseOperationContext {
+    public static final String SOURCE_BRANCH = "sourceBranch";
+    public static final String RULES = "rules";
+    public static final String OLD_RULES = "oldRules";
+    public static final String BASE_RELEASE_ID = "baseReleaseId";
+    public static final String IS_EMERGENCY_PUBLISH = "isEmergencyPublish";
+    public static final String BRANCH_RELEASE_KEYS = "branchReleaseKeys";
+}
