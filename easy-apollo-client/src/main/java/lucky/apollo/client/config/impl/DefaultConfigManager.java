9cabcf91ac12732d56053a4156652fa2df8f086c
luckylau <laujunbupt0203@163.com>
2022-02-28 22:04:00 +0800

    DefaultConfigManager

--- easy-apollo-client/src/main/java/lucky/apollo/client/config/impl/DefaultConfigManager.java
+++ easy-apollo-client/src/main/java/lucky/apollo/client/config/impl/DefaultConfigManager.java
@@ -1,22 +1,64 @@
 package lucky.apollo.client.config.impl;
 
-import lucky.apollo.client.config.Config;
-import lucky.apollo.client.config.ConfigFile;
-import lucky.apollo.client.config.ConfigManager;
+import com.google.common.collect.Maps;
+import lucky.apollo.client.build.ApolloInjector;
+import lucky.apollo.client.config.*;
 import lucky.apollo.common.constant.ConfigFileFormat;
+
+import java.util.Map;
 
 /**
  * @Author luckylau
  * @Date 2019/12/13
  */
 public class DefaultConfigManager implements ConfigManager {
+    private ConfigFactoryManager m_factoryManager;
+
+    private Map<String, Config> m_configs = Maps.newConcurrentMap();
+    private Map<String, ConfigFile> m_configFiles = Maps.newConcurrentMap();
+
+    public DefaultConfigManager() {
+        m_factoryManager = ApolloInjector.getInstance(ConfigFactoryManager.class);
+    }
+
     @Override
     public Config getConfig(String namespace) {
-        return null;
+        Config config = m_configs.get(namespace);
+
+        if (config == null) {
+            synchronized (this) {
+                config = m_configs.get(namespace);
+
+                if (config == null) {
+                    ConfigFactory factory = m_factoryManager.getFactory(namespace);
+
+                    config = factory.create(namespace);
+                    m_configs.put(namespace, config);
+                }
+            }
+        }
+
+        return config;
     }
 
     @Override
     public ConfigFile getConfigFile(String namespace, ConfigFileFormat configFileFormat) {
-        return null;
+        String namespaceFileName = String.format("%s.%s", namespace, configFileFormat.getValue());
+        ConfigFile configFile = m_configFiles.get(namespaceFileName);
+
+        if (configFile == null) {
+            synchronized (this) {
+                configFile = m_configFiles.get(namespaceFileName);
+
+                if (configFile == null) {
+                    ConfigFactory factory = m_factoryManager.getFactory(namespaceFileName);
+
+                    configFile = factory.createConfigFile(namespaceFileName, configFileFormat);
+                    m_configFiles.put(namespaceFileName, configFile);
+                }
+            }
+        }
+
+        return configFile;
     }
 }
